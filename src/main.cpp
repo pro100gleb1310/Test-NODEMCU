a6e2b7106b955ddd5530ab4cda87956ceb0bff17
pro100gleb1310 <mr.pro100gleb@gmail.com>
2023-03-18 17:18:26 +0200

    1commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//COMMIT 1
+#include <Arduino.h>
+
+void setup() {
+  pinMode(8, OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //
+}
